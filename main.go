7dd276340e729df195e40ff2fbb9e286b9e834c4
johnduls <fajarabdulloh07@gmail.com>
2023-12-02 16:43:20 +0700

    Initial Commit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/zanproject/go-restapi-gin/controllers/productcontroller"
 	models "github.com/zanproject/go-restapi-gin/setup"
 )
 
@@ -9,11 +10,11 @@ func main() {
 	r := gin.Default()
 	models.ConnectDatabase()
 
-	r.GET("/api/products", productController.Index)
-	r.GET("/api/products/:id", productController.Show)
-	r.POST("/api/products", productController.Create)
-	r.PUT("/api/products/:id", productController.Update)
-	r.DELETE("/api/products", productController.Delete)
+	r.GET("/api/products", productcontroller.Index)
+	r.GET("/api/products/:id", productcontroller.Show)
+	r.POST("/api/products", productcontroller.Create)
+	r.PUT("/api/products/:id", productcontroller.Update)
+	r.DELETE("/api/products", productcontroller.Delete)
 
 	r.Run()
 }
